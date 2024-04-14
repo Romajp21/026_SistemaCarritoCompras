26231dde7a8abb648d3901c7389b30fcb7079307
Romajp21 <34492781+Romajp21@users.noreply.github.com>
2024-04-13 22:01:06 -0600

    Revert "CarritodeCompras"
    This reverts commit 9c29a0de5f6b8cfff6a8d0af5bd1e1012b81cc6d.

--- ProyectoTest/Logica/Conexion.cs
+++ ProyectoTest/Logica/Conexion.cs
@@ -7,7 +7,6 @@ namespace ProyectoTest.Logica
 {
     public class Conexion
     {
-        //public static string CN = "Data Source=.;Initial Catalog=DB_CARRITO;Integrated Security=True";
-        public static string CN = "Server=ROMAJP\\DEVELOPER1;Database=DB_CARRITO;User ID=SA;Password=12345678;Trusted_Connection=True;TrustServerCertificate=True";
+        public static string CN = "Data Source=.;Initial Catalog=DB_CARRITO;Integrated Security=True";
     }
 }
